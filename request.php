df12aa471712b0e7ea8f82754acc7bd01f12a6e1
AHMEDBASHIR999 <147585277+AHMEDBASHIR999@users.noreply.github.com>
2024-06-07 18:08:57 +0500

    Update request.php

--- request.php
+++ request.php
@@ -2,5 +2,5 @@
 header('Content-type: text/xml');
 ?>
 <Response>
-    <Dial callerId="+12029526015"><?php  echo $_POST['To'];?></Dial>
+    <Dial callerId="+18288091746"><?php  echo $_POST['To'];?></Dial>
 </Response>
